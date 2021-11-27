dd16aa668222f479c1e3b690a62cd50c431b9516
ivancio <ivancio87@gmail.com>
2021-11-27 02:11:08 -0300

    Initial commit

---
+++ app/src/main/java/com/example/appdonacion/MainActivity.java
@@ -0,0 +1,86 @@
+package com.example.appdonacion;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+public class MainActivity extends AppCompatActivity {
+
+    private EditText correo;
+    private EditText contrasena;
+    private FirebaseAuth mAuth;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        correo = findViewById(R.id.correo);
+        contrasena = findViewById(R.id.contrasena);
+
+
+        // Initialize Firebase Auth
+        mAuth = FirebaseAuth.getInstance();
+
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        // Check if user is signed in (non-null) and update UI accordingly.
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        if(currentUser != null){
+            currentUser.reload();
+        }
+    }
+
+    public void iniciarSesion(View view){
+        mAuth.signInWithEmailAndPassword(correo.getText().toString(), contrasena.getText().toString())
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            //Log.d(TAG, "signInWithEmail:success");
+                            FirebaseUser user = mAuth.getCurrentUser();
+                            Toast.makeText(getApplicationContext(), "Ha iniciado sesion correctamente",Toast.LENGTH_SHORT).show();
+                            Intent i = new Intent(getApplicationContext(), PaginaPrincipalActivity.class);
+                            startActivity(i);
+                            //updateUI(user);
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            //Log.w(TAG, "signInWithEmail:failure", task.getException());
+                            Toast.makeText(getApplicationContext(), "Error: compruebe su usuario y contraseña",
+                                    Toast.LENGTH_SHORT).show();
+                            //updateUI(null);
+                        }
+                    }
+                });
+
+    }
+
+
+    public void irIniciar(View view){
+        Intent i = new Intent(this, PaginaPrincipalActivity.class);
+        startActivity(i);
+
+    }
+
+    public void irRegistrarse(View view){
+        Intent i = new Intent(this, RegistrarseActivity.class);
+        startActivity(i);
+
+    }
+}
---
+++ app/src/main/java/com/example/appdonacion/PaginaPrincipalActivity.java
@@ -0,0 +1,14 @@
+package com.example.appdonacion;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PaginaPrincipalActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pagina_principal);
+    }
+}
