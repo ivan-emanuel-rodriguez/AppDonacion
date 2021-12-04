13e789a5a645232071380f041752ed1c85118bf3
Danieltq04 <daniel.tq04@gmail.com>
2021-12-04 12:24:13 -0300

    función cambiar idioma

--- app/src/main/java/com/example/appdonacion/ui/slideshow/SlideshowFragment.java
+++ app/src/main/java/com/example/appdonacion/ui/slideshow/SlideshowFragment.java
@@ -1,9 +1,12 @@
 package com.example.appdonacion.ui.slideshow;
 
+import android.content.Intent;
+import android.content.res.Configuration;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -12,17 +15,31 @@ import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
 
+import com.example.appdonacion.PaginaPrincipalActivity;
 import com.example.appdonacion.R;
+
+import java.util.Locale;
 
 public class SlideshowFragment extends Fragment {
 
     private SlideshowViewModel slideshowViewModel;
 
+    private Button ingles;
+    private Button espaniol;
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
+
+
+
         slideshowViewModel =
                 ViewModelProviders.of(this).get(SlideshowViewModel.class);
         View root = inflater.inflate(R.layout.fragment_slideshow, container, false);
+        ingles = (Button) root.findViewById(R.id.btn_ingles);
+        espaniol = (Button) root.findViewById(R.id.btn_esp);
+        ingles.setOnClickListener(CambiarAEs);
+        espaniol.setOnClickListener(ChangeToEn);
+
+
         final TextView textView = root.findViewById(R.id.text_slideshow);
         slideshowViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
             @Override
@@ -31,5 +48,25 @@ public class SlideshowFragment extends Fragment {
             }
         });
         return root;
+    }
+    private View.OnClickListener CambiarAEs = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            CambiarIdioma("en");
+        }
+    };
+    private View.OnClickListener ChangeToEn = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            CambiarIdioma("es");
+        }
+    };
+    public void CambiarIdioma(String idioma){
+        Locale locale;
+        Configuration config = new Configuration();
+        locale = new Locale(idioma);
+        config.locale =locale;
+        getResources().updateConfiguration(config, null);
+        getActivity().recreate();
     }
 }
--- app/src/main/java/com/example/appdonacion/ui/slideshow/SlideshowViewModel.java
+++ app/src/main/java/com/example/appdonacion/ui/slideshow/SlideshowViewModel.java
@@ -1,8 +1,24 @@
 package com.example.appdonacion.ui.slideshow;
 
+import android.app.ProgressDialog;
+import android.os.Bundle;
+//import android.view.View;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
+
+import com.example.appdonacion.DonacionSharePreferences;
+import com.example.appdonacion.R;
+import com.google.firebase.storage.FirebaseStorage;
 
 public class SlideshowViewModel extends ViewModel {
 
@@ -10,10 +26,13 @@ public class SlideshowViewModel extends ViewModel {
 
     public SlideshowViewModel() {
         mText = new MutableLiveData<>();
-        mText.setValue("This is slideshow fragment");
+        //mText.setValue("Configuraciones");
     }
 
     public LiveData<String> getText() {
         return mText;
     }
+
+
+
 }
